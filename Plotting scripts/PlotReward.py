ddbf9c754e51ad3a73e5bf324abaa513caf83353
TimNiklasWitte <wittet@uos.de>
2023-08-04 17:17:30 +0200

    Add plotting scripts

---
+++ Plotting scripts/PlotReward.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.append("../")
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+from LoadDataframe import *
+
+def main():
+    
+    df = load_dataframe()
+
+    #
+    # Plotting
+    #
+
+    fig, axes = plt.subplots(1, 2, figsize=(10,5))
+
+    sns.lineplot(data=df.loc[:, ["avg reward", "avg reward network only"]], ax=axes[0], palette=['r', 'g']).set(title='Average reward')
+    sns.lineplot(data=df.loc[:, ["sum reward", "sum reward network only"]], ax=axes[1], palette=['r', 'g']).set(title='Sum reward')
+
+    fig.suptitle('Reward')
+    fig.tight_layout()
+
+    # grid
+    for ax in axes.flatten():
+        ax.grid()
+
+    plt.savefig("./Plots/Reward.png")
+    plt.show()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt received")
---
+++ Plotting scripts/PlotTrainTestLoss.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.append("../")
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+from LoadDataframe import *
+
+def main():
+
+    df = load_dataframe()
+
+    fig, axes = plt.subplots(1, 3, figsize=(8,5))
+
+    sns.lineplot(data=df.loc[:, ["train loss", "test loss"]], ax=axes[0])
+    sns.lineplot(data=df.loc[:, ["train policy loss", "test policy loss"]], ax=axes[1])
+    sns.lineplot(data=df.loc[:, ["train value loss", "test value loss"]], ax=axes[2])
+
+    fig.suptitle('Train and test loss')
+    fig.tight_layout()
+
+    # grid
+    for ax in axes.flatten():
+        ax.grid()
+
+    plt.savefig("./Plots/TrainTestLoss.png")
+    plt.show()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt received")
